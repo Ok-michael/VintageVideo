ad749fe20bc1f8da215a1762741b27cbcef7b6ff
Ok-michael <okomichaelakpu2@gmail.com>
2024-09-16 03:48:11 +0100

    In this commit, I have created three serializers (OrderItemSerializer, OrderSerializer, OrderCustomerSerializer), some class based views (OrderListAPIBaseView, OrderByCustomerView, IncompleteOrdersByCustomerView, OrderByStatusView, CreateOrderView), function based views (cancel_order, set_status) and setup the URls for the . I also generated a token for calling this API from VVG

--- orders/main/exceptions.py
+++ orders/main/exceptions.py
@@ -8,6 +8,10 @@ class OrderAlreadyCompletedError(Exception):
         message = f'The order with ID: {order.id} is already completed.'
         super().__init__(message)
 
+class OrderAlreadyCancelledError(Exception): 
+    def __init__(self, order): 
+        message = f'The order with ID: {order.id} is already cancelled.' 
+        super().__init__(message)
 
 class OrderCancellationError(Exception):
     pass
